085fad6e616a4edc23d934b597919e576fa53329
Ben Davies <me@bdavies.net>
2021-12-30 12:54:26 +0000

    docs: Add documentation to the main class

--- src/main/java/net/bdavies/mc/MovieCatalogApplication.java
+++ src/main/java/net/bdavies/mc/MovieCatalogApplication.java
@@ -3,9 +3,20 @@ package net.bdavies.mc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Class of the Application this is where Spring will boostrap and
+ * setup all of the internals
+ *
+ * @author ben.davies
+ */
 @SpringBootApplication
 public class MovieCatalogApplication {
 
+	/**
+	 * Main Function of the application
+	 *
+	 * @param args The command line arguments supplied from the user
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogApplication.class, args);
 	}
