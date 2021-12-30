0a00fcbe32113bc9bdeaedc6f0ab33463cb11155
Ben Davies <me@bdavies.net>
2021-12-30 17:12:12 +0000

    feat: Setup Junit4 Testing

---
+++ src/test/java/net/bdavies/mc/SmokeTest.java
@@ -0,0 +1,15 @@
+package net.bdavies.mc;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.Test;
+
+public class SmokeTest
+{
+
+	@Test
+	public void smoke() {
+		assertThat(true).isTrue();
+	}
+
+}
