32fc78dfe8579ce1898458f923ffd217dfbda162
Ben Davies <me@bdavies.net>
2021-12-30 16:36:24 +0000

    feat: Add a find by email to the DirectorRepository

--- src/main/java/net/bdavies/mc/repositories/DirectorRepository.java
+++ src/main/java/net/bdavies/mc/repositories/DirectorRepository.java
@@ -1,6 +1,7 @@
 package net.bdavies.mc.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import net.bdavies.mc.models.Director;
@@ -14,4 +15,11 @@ import net.bdavies.mc.models.Director;
 @RepositoryRestResource
 public interface DirectorRepository extends CrudRepository<Director, Integer>
 {
+	/**
+	 * Find a Director by email
+	 *
+	 * @param email the email to check
+	 * @return a director based on the email or null if not found
+	 */
+	Director findByEmail(@Param("email") String email);
 }
